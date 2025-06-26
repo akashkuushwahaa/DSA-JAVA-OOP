07e1e4d976ce422d829e660bfd8974208a4cd102
akashkuushwahaa <akashkushwaha76986@gmail.com>
2025-06-26 16:37:35 +0530

    Max Wealth - LeetCode

---
+++ Linear Search/MaxWealth.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/richest-customer-wealth/
+public class MaxWealth{
+    public static void main(String[] args){
+
+    }
+    
+        public int maximumWealth(int[][] accounts) {
+        
+    }
+}
