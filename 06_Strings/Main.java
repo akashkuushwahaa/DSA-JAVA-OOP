08f9c76c87a6e5377c2fb7b940d548a1622303bc
akashkuushwahaa <akashkushwaha76986@gmail.com>
2026-03-20 10:24:04 +0530

    Strings

---
+++ 06_Strings/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 4, 19};
+        int num = 10;
+        String name = "Akash Kushwaha";
+//        System.out.println(name);
+
+        String a = "Akash";
+        System.out.println(a);
+        a = "Kushwaha";
+        System.out.println(a);
+    }
+}
